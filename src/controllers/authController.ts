662f718cdbf26918880e4269553c3f0f6fbaa48c
Eddie Taliaferro <eddielacrosse2@gmail.com>
2026-01-12 21:26:57 -0500

    Re-added Nodenamiler p2

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -8,8 +8,8 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true, // use SSL
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.VERCEL_EMAIL_USER,
+    pass: process.env.VERCEL_EMAIL_PASS,
   },
 });
 
